63da309d5c073bac12425dca0ebf2692efbb4d5b
Joshua Michael Hertlein <jmhertlein@gmail.com>
2016-01-10 01:16:47 -0600

    added "if not exists" to all table creations

--- src/main/resources/setup.sql
+++ src/main/resources/setup.sql
@@ -1,23 +1,23 @@
-CREATE TABLE Item(
+CREATE TABLE IF NOT EXISTS Item(
   item_name TEXT PRIMARY KEY
 );
 
-CREATE TABLE Turn(
+CREATE TABLE IF NOT EXISTS Turn(
   turn_id SERIAL PRIMARY KEY,
   item_name TEXT REFERENCES Item(item_name),
   open_ts TIMESTAMP NOT NULL DEFAULT now(),
   close_ts TIMESTAMP
 );
 
-CREATE TABLE Trade(
+CREATE TABLE IF NOT EXISTS Trade(
   turn_id INTEGER REFERENCES Turn(turn_id),
   trade_ts TIMESTAMP NOT NULL DEFAULT now(),
   price INTEGER NOT NULL,
   quantity INTEGER NOT NULL,
-  PRIMARY KEY(turn_id, order_ts)
+  PRIMARY KEY(turn_id, trade_ts)
 );
 
-CREATE TABLE Quote(
+CREATE TABLE IF NOT EXISTS Quote(
   item_name TEXT REFERENCES Item(item_name),
   quote_ts TIMESTAMP NOT NULL DEFAULT now(),
   bid1 INTEGER NOT NULL,
