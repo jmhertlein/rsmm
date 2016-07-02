d492c1cdd7f6f17a09a6be6832ccb7621d76f3a7
Joshua Michael Hertlein <jmhertlein@gmail.com>
2016-07-02 01:56:07 -0500

    add about section

--- ruby/webdash/app/controllers/about_controller.rb
+++ ruby/webdash/app/controllers/about_controller.rb
@@ -1,2 +1,11 @@
 class AboutController < ApplicationController
+
+  def index
+    @josh_age = age Time.parse('1992-11-21')
+  end
+
+  def age(dob)
+    now = Time.now.utc.to_date
+    now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
+  end
 end
--- ruby/webdash/config/routes.rb
+++ ruby/webdash/config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # Example of regular route:
   get 'trading' => 'trading#index'
   get 'processes' => 'process#index'
+  get 'about' => 'about#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
