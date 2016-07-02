e1496dd928d06cae0d0dcac150e42b26a9324128
Joshua Michael Hertlein <jmhertlein@gmail.com>
2016-07-02 01:05:15 -0500

    fleshed out controller

--- ruby/webdash/app/controllers/process_controller.rb
+++ ruby/webdash/app/controllers/process_controller.rb
@@ -1,8 +1,8 @@
 class ProcessController < ApplicationController
   def index
-    @items_tracked = 0
-    @last_ge_update_ts = "00:00:00"
-    @current_ge_date = "2016-07-1"
-    @limits_tracked = 0
+    @items_tracked = Price.connection.select_all("select count(item_id) from item;")[0]["count"].to_s
+    @last_ge_update_ts = Time.parse(Price.connection.select_all("select max(created_ts) from price;")[0]["max"]).strftime("%T")
+    @current_ge_date = Price.connection.select_all("select max(day) from price")[0]["max"].to_s
+    @limits_tracked = Price.connection.select_all("select count(item_id) from item where ge_limit is not null")[0]["count"].to_s
   end
 end
