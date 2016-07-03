24777817c4342a268f8f8a11118188e9194f3851
Josh Hertlein <jmhertlein@gmail.com>
2016-07-02 21:15:40 -0500

    fix calc of notional

--- ruby/webdash/app/controllers/trading_controller.rb
+++ ruby/webdash/app/controllers/trading_controller.rb
@@ -8,11 +8,13 @@ class TradingController < ApplicationController
     @total_notional = Price.connection.select_all("select coalesce(sum(abs(quantity)*price),0) from trade")[0]["coalesce"]
 
     @daily_closed = Price.connection.select_all("select coalesce(sum(quantity * price * -1),0) as closed_profit from turn natural join trade where close_ts > now()::date;").first["closed_profit"]
-    @daily_volume = Price.connection.select_all("select coalesce(sum(abs(quantity)),0) from trade where trade_ts > now()::date;")[0]["coalesce"]
-    @daily_notional = Price.connection.select_all("select coalesce(sum(abs(quantity)*price),0) from trade where trade_ts > now()::date;")[0]["coalesce"]
-    @daily_closed_volume = Price.connection.select_all("select coalesce(sum(quantity*price*-1),0) from turn natural join trade where close_ts > now()::date;")[0]["coalesce"]
+    @daily_notional = Price.connection.select_all("select coalesce(sum(abs(quantity)*price),0) from trade natural join turn where close_ts > now()::date;")[0]["coalesce"]
+    @daily_notional = @daily_notional.to_i / 2
+    @daily_closed_volume = Price.connection.select_all("select coalesce(sum(abs(quantity)),0) from turn natural join trade where close_ts > now()::date;")[0]["coalesce"]
+    @daily_volume = @daily_closed_volume.to_i / 2
     if @daily_closed_volume.to_i != 0
       @daily_avg_profit_per_item = @daily_closed.to_i / (@daily_closed_volume.to_f/2)
+      @daily_avg_profit_per_item = @daily_avg_profit_per_item.round(2)
     else
       @daily_avg_profit_per_item = "0"
     end
