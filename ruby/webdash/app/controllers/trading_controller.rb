3eef4a67e68ec24c155b29f28ed537ea77b11679
Joshua Michael Hertlein <jmhertlein@gmail.com>
2016-08-24 21:21:08 -0500

    Expanded daily stats to past 5 days

--- ruby/webdash/app/controllers/trading_controller.rb
+++ ruby/webdash/app/controllers/trading_controller.rb
@@ -1,27 +1,27 @@
 include ActionView::Helpers::NumberHelper
 
 class TradingController < ApplicationController
-  helper_method :commas
+  helper_method :commas, :gp_fmt
   def index
     @total_profit = Price.connection.select_all("SELECT SUM(price * (quantity*-1)) AS total_closed FROM Trade NATURAL JOIN Turn WHERE close_ts IS NOT NULL;")[0]["total_closed"]
     @total_volume = Price.connection.select_all("select sum(abs(quantity)) from trade")[0]["sum"]
-    @total_notional = Price.connection.select_all("select coalesce(sum(abs(quantity)*price),0) from trade")[0]["coalesce"]
-
-    @daily_closed = Price.connection.select_all("select coalesce(sum(quantity * price * -1),0) as closed_profit from turn natural join trade where close_ts > now()::date;").first["closed_profit"]
-    @daily_notional = Price.connection.select_all("select coalesce(sum(abs(quantity)*price),0) from trade natural join turn where close_ts > now()::date;")[0]["coalesce"]
-    @daily_notional = @daily_notional.to_i / 2
-    @daily_closed_volume = Price.connection.select_all("select coalesce(sum(abs(quantity)),0) from turn natural join trade where close_ts > now()::date;")[0]["coalesce"]
-    @daily_volume = @daily_closed_volume.to_i / 2
-    if @daily_closed_volume.to_i != 0
-      @daily_avg_profit_per_item = @daily_closed.to_i / (@daily_closed_volume.to_f/2)
-      @daily_avg_profit_per_item = @daily_avg_profit_per_item.round(2)
-    else
-      @daily_avg_profit_per_item = "0"
-    end
-    @quotes = Price.connection.select_all("select count(*) from quote where quote_ts > now()::date")[0]["count"]
+    @total_notional = Price.connection.select_all("select coalesce(sum(abs(quantity)*price)/2,0) from trade")[0]["coalesce"]
+    
+    @daily_stats = Price.connection.select_all("select * from daily_history order by day desc limit 5")
   end
 
   def commas s
     return number_with_delimiter(s, :delimiter => ',')
+  end
+
+  def gp_fmt s
+    gp = s.to_i
+    if gp < 100000
+      return number_with_delimiter(gp, :delimiter => ',')
+    elsif gp < 10000000
+      return "#{number_with_delimiter(gp/1000, :delimiter => ',')}k"
+    else
+      return "#{number_with_delimiter(gp/1000000, :delimiter => ',')}M"
+    end
   end
 end
