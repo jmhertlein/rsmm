08575300ffaf415c3582c1cb6e1f8f65cc40d153
Joshua Michael Hertlein <jmhertlein@gmail.com>
2017-03-06 00:05:38 -0600

    split up configs into prod and test

--- ruby/trade-config/lib/trade-config.rb
+++ ruby/trade-config/lib/trade-config.rb
@@ -1,18 +1,44 @@
 module TradeConfig
-  DB_INFO = {
-    host: "claudius",
-    port: 5432,
-    dbname: "rsmm",
-    user: "rsmm",
-    sslmode: "require",
-    sslcert: File.join(Dir.home, ".postgresql", "rsmm.crt"),
-    sslkey: File.join(Dir.home, ".postgresql", "rsmm.key"),
-    sslrootcert: File.join(Dir.home, ".postgresql", "root.crt")
+  CFG = {
+    prod: {
+      db: {
+        host: "claudius",
+        port: 5432,
+        dbname: "rsmm",
+        user: "rsmm",
+        sslmode: "require",
+        sslcert: File.join(Dir.home, ".postgresql", "rsmm.crt"),
+        sslkey: File.join(Dir.home, ".postgresql", "rsmm.key"),
+        sslrootcert: File.join(Dir.home, ".postgresql", "root.crt")
+      }, 
+      mail: {
+        sender_host: "trade.jmhertlein.net",
+        recipients: "jmhertlein@gmail.com",
+        mail_host: "trade.jmhertlein.net",
+      }
+    },
+
+    dev: {
+      db: {
+        host: "localhost",
+        port: 5432,
+        dbname: "rsmm",
+        user: "rsmm",
+        sslmode: "allow",
+      }, 
+      mail: {
+        sender_host: "trade.jmhertlein.net",
+        recipients: "jmhertlein@gmail.com",
+        mail_host: "trade.jmhertlein.net",
+      }
+    }
   }
 
-  MAIL_INFO = {
-    sender_host: "trade.jmhertlein.net",
-    recipients: "jmhertlein@gmail.com",
-    mail_host: "trade.jmhertlein.net",
-  }
+  def self.for mode, svc, key=nil
+    if key.nil?
+      return CFG[mode][svc]
+    else
+      return CFG[mode][svc][key]
+    end
+  end
 end
