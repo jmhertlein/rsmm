509766848930616524a019cac19b776c9ac6a9ba
Joshua Michael Hertlein <jmhertlein@gmail.com>
2017-08-05 21:44:50 -0500

    added rstype dialog

---
+++ java/src/main/java/cafe/josh/rsmm/model/RSType.java
@@ -0,0 +1,6 @@
+package cafe.josh.rsmm.model;
+
+public enum RSType {
+    RS3,
+    OSRS;
+}
---
+++ java/src/main/java/cafe/josh/rsmm/viewfx/RSTypeDialog.java
@@ -0,0 +1,36 @@
+package cafe.josh.rsmm.viewfx;
+
+import cafe.josh.joshfx.FXMLDialog;
+import cafe.josh.rsmm.model.RSType;
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleGroup;
+
+public class RSTypeDialog extends FXMLDialog {
+    @FXML
+    private RadioButton osrsButton;
+    @FXML
+    private RadioButton rs3Button;
+
+    private final ToggleGroup group;
+
+    public RSTypeDialog() {
+        super("/fxml/rs_type_chooser.fxml");
+        group = new ToggleGroup();
+        osrsButton.setToggleGroup(group);
+        rs3Button.setToggleGroup(group);
+
+        osrsButton.setUserData(RSType.OSRS);
+        rs3Button.setUserData(RSType.RS3);
+    }
+
+    public RSType getSelectedType() {
+        Toggle b = group.getSelectedToggle();
+        if(b == null) {
+            return null;
+        }
+
+        return (RSType) b.getUserData();
+    }
+}
